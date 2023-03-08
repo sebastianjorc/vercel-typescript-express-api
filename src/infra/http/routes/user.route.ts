ed4d0bc29dc73633ec81afa521aeacf0a6e882ad
Sebastian <sebastianjorc@gmail.com>
2023-03-08 00:26:34 -0300

    add CRUD user

--- src/infra/http/routes/user.route.ts
+++ src/infra/http/routes/user.route.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { MongoError } from 'mongodb';
 import User from './../../../models/user.model';
-//import {userController} from './../controllers/user.controller'
 export const userRoute = Router();
 
 userRoute.get('/', async (req : Request, res : Response) => {
@@ -21,11 +20,64 @@ userRoute.post('/', async (req : Request, res : Response) => {
     .save()
     .then((data: any) => res.json(data))
     .catch((error : MongoError)=>res.json({message:`${error}    ${req.body}`}));
-});//userController.createUser);
+});
 
 
-/*
-userRoute.get('/:id', userController.getUser);
-userRoute.put('/:id', userController.editUser);
-userRoute.delete('/:id', userController.editUser);
-*/
+userRoute.patch('/:id/avatar', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, { $set: { avatarPath: req.body.avatarPath } }, { new: true }, (err, doc) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'No se pudo actualizar el usuario' });
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+userRoute.patch('/:id/username', (req, res) => {
+  console.log('REQUEST: \n\n\n',req.body);
+  User.findByIdAndUpdate(req.params.id, { $set: { username: req.body.username } }, { new: true }, (err, doc) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'No se pudo actualizar el usuario' });
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+userRoute.patch('/:id/email', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, { $set: { email: req.body.email } }, { new: true }, (err, doc) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'No se pudo actualizar el usuario' });
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+userRoute.patch('/:id/password', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, { $set: { password: req.body.password } }, { new: true }, (err, doc) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'No se pudo actualizar el usuario' });
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+userRoute.get('/check-email/:email', async (req, res) => {
+  const email = req.params.email;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(200).json({ exists: true });
+    } else {
+      res.status(200).json({ exists: false });
+    }
+  } catch (error) {
+    res.status(500).json({ error: `Error al buscar el usuario: ${error}` });
+  }
+});
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,17 +6,17 @@ export interface IUserInput{
     username    :string;
     email       :string;
     password    :string;
+    level       :number;
 }
 
 export interface IUserLoggin{
   email    :string;
-  password    :string;
+  password :string;
 }
 
 export interface UserDocument extends IUserInput, mongoose.Document {
-  createdAt: Date;
-  updatedAt: Date;
-  comparePassword(candidatePassword: String): Promise<boolean>;//
+  createdAt: Date;  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>;//
 }
 
 const userLogginSchema = new mongoose.Schema<IUserLoggin>({
@@ -30,24 +30,16 @@ const userSchema = new mongoose.Schema<IUserInput>(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true },
+    level: { type: Number, default: 0 }
   },
-  {
-    timestamps: true
-  }
+  { timestamps: true  }
 );
 
 userSchema.pre('save', async function (next) {
-  let user = this as UserDocument;
+  const user = this as unknown as UserDocument;
   if (!user.isModified('password')) {
     return await next();
   }
-  //console.log("\n\n pre error \n\n");
- // aquí debajo está el error
-  //const salt = await bcrypt.genSalt(config.get('saltWorkFactor'));
-  // hash and replace password
-  //const hash = await bcrypt.hashSync(user.password, salt);
-  //user.password = hash;
-  //console.log("\n\npost salt error: "+user.password+"\n\n");
   return await next();
 });
 
@@ -57,30 +49,9 @@ userSchema.methods.comparePassword = async function (
   const user = this as UserDocument;
 
   return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
-};/**/
+};
 
 const UserModel = mongoose.model<UserDocument>('User', userSchema);
 export const userLogginModel = mongoose.model('UserLoggin', userLogginSchema);
 
 export default UserModel;
-
-/*import mongoose from 'mongoose';
-
-export interface IUser extends mongoose.Document{
-    avatarPath:String;
-    username:String;
-    email:String;
-    password:String;
-}
-
-export const userSchema = new mongoose.Schema({
-    avatarPath: { type: String, required: true },
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    somethingElse: String,
-});
-
-const User = mongoose.model<IUser>('User', userSchema);
-export default User;
-*/
