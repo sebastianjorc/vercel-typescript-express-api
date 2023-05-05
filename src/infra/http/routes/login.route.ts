fb4641c077b80527bd44b66a900298f52d62b4b7
Sebastian <sebastianjorc@gmail.com>
2023-05-05 19:43:17 -0400

    FIX

--- src/infra/http/routes/login.route.ts
+++ src/infra/http/routes/login.route.ts
@@ -4,20 +4,16 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export const loginRoute = Router();
-
 loginRoute.post('/', async (req : Request, res : Response) => {
   try{
-    const {email, password} = req.body;
-    const userEmail = email.toLowerCase();
-    const user = await User.findOne({ userEmail });
+    const { email, password } = req.body;
+    const userEmail = email.toLowerCase(); // Convertir correo electrónico a minúsculas
+    const user = await User.findOne({ email: userEmail });
     if (!user) {
       return res.status(400).send({ msg: 'Usuario no existe en la base de datos' });
     }
-    //const isPasswordCorrect = await bcrypt.compare(user.password, password);
-    if (password!=user.password){//isPasswordCorrect) {
-      return res.status(400)
-      .send({ msg: `Contraseña '${password}' inválida para ${userEmail}:${user.password}
-      \n\n ${email} => ${userEmail} == ${user.email}` });
+    if (password != user.password) {
+      return res.status(400).send({ msg: `Contraseña '${password}' inválida para ${email}:${user.password}` });
     }
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET || 'secret');
     res.send({ token, user });
